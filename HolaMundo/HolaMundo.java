a8a9a4b601cb9aa5f1914d2ff599e1603c5a67a6
Demonassit <demonassit@hotmail.com>
2020-10-13 18:04:44 -0500

    Ejemplo de Hola mundo

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,38 @@
+// asi es como podemos comentar una linea
+
+/*   
+para poder comentar varias linea
+
+a la vez
+
+y poder escribir mas de una sola linea 
+
+de comentario
+
+*/
+
+class HolaMundo{
+    //aqui es donde va estar todo nuestro codigo de programacion
+    //esto es el cuerpo del programa
+
+    //clase principal
+
+    public static void main(String[] args){
+        /*
+        este es el metodo principal de cualquier clase de java
+
+        en el cual se especifica que va a poder recibir cadenas como
+        argumentos 
+
+        los argumentos son cualquier tipo de valor que puede recibir 
+        este metodo principal, aqui es donde nosotro especificamos todo
+        el cuerpo del algoritmo
+        */ 
+
+        System.out.print("Hola Mundo, ya no eres kawaii");
+        //Esto es lo mismo que:
+        //Escribir "Hola Mundo ¬¬, ya no eres kawaii"
+    } 
+}
+
+

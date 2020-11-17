dbcba8e9bf4583666fc488336cfa762c8c9eb5d4
Demonassit <demonassit@hotmail.com>
2020-11-17 17:25:29 -0600

    clase principal herencia

---
+++ Herencia/Principal.java
@@ -0,0 +1,13 @@
+/*
+va a ser la clase principal para poder hacer las invocaciones de los difeerentes
+objetos de clase y sus metodos de un reino animal
+
+*/
+
+public class Principal{
+
+    //al metodo principal de todo el programa
+    public static void main(String[] args){
+        //y ahora no ponemos anda y utilizare git como se debe 
+    }
+}

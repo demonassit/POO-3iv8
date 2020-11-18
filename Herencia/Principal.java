fe186e32059aeb835a9fb65d231ac6faab244fda
Demonassit <demonassit@hotmail.com>
2020-11-17 18:07:28 -0600

    cambios de dog y cat

--- Herencia/Principal.java
+++ Herencia/Principal.java
@@ -9,5 +9,19 @@ public class Principal{
     //al metodo principal de todo el programa
     public static void main(String[] args){
         //y ahora no ponemos anda y utilizare git como se debe 
+
+        //ahora vamos a realizar los objetos de las clases y sus invocaciones
+
+        Perro dog = new Perro("Teddy", "Callejero", "Croquetas", 2, "fuerte");
+        Gato cat = new Gato("Miau", "Hogareño", "atun", 1, 7);
+
+        //dog.setNombre(variable);
+
+        //los metodos
+        dog.mostrarPerro();
+        System.out.println("------");
+        cat.mostrarGato();
+        System.out.println("---adorno *w* uwu ono ---");
+        
     }
 }
